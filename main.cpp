abec21b67fedf1e03cf2cd75e0613418e42a07cf
vamaria06-ux <mashaulanova545@gmail.com>
2025-12-01 17:48:05 +0300

    main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ namespace top
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
+  void make_f (IDraw**b,size_t k);
+  void get_points (IDraw*b, p_t ** ps, size_t &s);
+  frame_t build_frame(const p_t * ps, size_t s);
+  char * build_canvas(frame_t f);
+  void paint_canvas(char * cnv,frame_t fv, const p_t * ps, size_t k, char f);
+  void print_canvas(const char * cnv, frame_t fv);
   bool operator == (p_t a, p_t b)
   {
     return a.x == b.x && a.y == b.y;
@@ -29,6 +35,31 @@ namespace top
 int main()
 {
   using namespace top;
+  IDraw * f[3] = {};
+  p_t * p = nullptr;
+  size_t s = 0;
+  int err = 0;
+  try
+  {
+    make_f(f,3);
+    for (size_t i = 0; i < 3, ++i)
+    {
+      get_points(f[i],&p,s);
+    }
+    frame_t fv= build_frame(p,s);
+    cnv = build_canvas(fv)
+    paint_canvas(cnv, fv, p, s , '#');
+    print_canvas(cnv, fv);
+  } catch()
+  {
+    err = 1;
+  {
+  delete f[0];
+  delete f[1];
+  delete f[2];
+  delete[] p;
+  delete[] cnv;
+  return err;
 }
 top::Dot::Dot(int x, int y) :
   IDraw(),
