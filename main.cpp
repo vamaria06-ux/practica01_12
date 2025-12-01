e6987b7a638017329c2342f86996ff2da91c9670
vamaria06-ux <mashaulanova545@gmail.com>
2025-12-01 17:17:04 +0300

    add srtuct Dot

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-namespace top {
+namespace top
+{
   struct p_t
   {
     int x,y;
   };
   struct IDraw
   {
-    virtual p_t begin() = 0;
-    virtual p_t next(p_t) = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
   };
   bool operator == (p_t a, p_t b)
   {
@@ -17,7 +18,28 @@ namespace top {
   {
     return !(a == b);
   }
+  struct Dot :: IDraw
+  {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+  };
 }
-int main(){
+int main()
+{
   using namespace top;
+}
+top::Dot::Dot(int x, int y) :
+  IDraw(),
+  o{x,y}
+{
+}
+top::p_t top::Dot::begin() const
+{
+  return o;
+}
+top::p_t top::Dot::next(p_t) const
+{
+  return begin();
 }
