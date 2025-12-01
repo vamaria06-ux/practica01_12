4369b191e087ef49d36e902437e1b9d75daad355
vamaria06-ux <mashaulanova545@gmail.com>
2025-12-01 20:32:30 +0300

    fixed compilation errors

--- main.cpp
+++ main.cpp
@@ -10,8 +10,13 @@ namespace top
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
-  void make_f (IDraw**b,size_t k);
-  void get_points (IDraw*b, p_t ** ps, size_t &s);
+  struct frame_t
+  {
+    p_t left_bot;
+    p_t right_top;
+  };
+  void make_f (IDraw** b,size_t k);
+  void get_points (IDraw* b, p_t ** ps, size_t &s);
   frame_t build_frame(const p_t * ps, size_t s);
   char * build_canvas(frame_t f);
   void paint_canvas(char * cnv,frame_t fv, const p_t * ps, size_t k, char f);
@@ -24,7 +29,7 @@ namespace top
   {
     return !(a == b);
   }
-  struct Dot :: IDraw
+  struct Dot:IDraw
   {
     p_t begin() const override;
     p_t next(p_t) const override;
@@ -38,22 +43,23 @@ int main()
   IDraw * f[3] = {};
   p_t * p = nullptr;
   size_t s = 0;
+  char * cnv = nullptr;
   int err = 0;
   try
   {
     make_f(f,3);
-    for (size_t i = 0; i < 3, ++i)
+    for (size_t i = 0; i < 3; ++i)
     {
       get_points(f[i],&p,s);
     }
     frame_t fv= build_frame(p,s);
-    cnv = build_canvas(fv)
+    cnv = build_canvas(fv);
     paint_canvas(cnv, fv, p, s , '#');
     print_canvas(cnv, fv);
-  } catch()
+  } catch(...)
   {
     err = 1;
-  {
+  }
   delete f[0];
   delete f[1];
   delete f[2];
