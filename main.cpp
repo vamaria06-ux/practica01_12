d376ca018e0f2a16d95275616dc50cddee479397
vamaria06-ux <mashaulanova545@gmail.com>
2025-12-08 16:52:55 +0300

    frame

--- main.cpp
+++ main.cpp
@@ -88,6 +88,26 @@ size_t top::points(const IDraw& d, p_t** pts, size_t s)
   }
   return delta;
 }
+top::f_t top::frame(const p_t* pts, size_t s)
+{
+  if (!s)
+  {
+    throw std::logic_error("bad size");
+  }
+  int minx = pts[0].x, maxx= minx;
+  int miny = pts[0].y, maxy = miny;
+  for (size_t i=1; i<s;i++)
+  {
+    minx= std::min(minx,pts[i].x);
+    maxx= std::max(maxx, pts[i].x);
+    miny = std::min(miny, pts[i].y);
+    maxy = std::max(maxy, pts[i].y);
+  }
+  p_t aa(minx,miny);
+  p_t bb(maxx,maxy);
+  return {aa,bb};
+}
+
 top::Dot::Dot(int x, int y) :
   IDraw(),
   o{x,y}
