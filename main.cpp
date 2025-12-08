06939dbf194b022f61ffe522b7f7120acd9276b6
vamaria06-ux <mashaulanova545@gmail.com>
2025-12-08 16:45:55 +0300

    points extend

--- main.cpp
+++ main.cpp
@@ -67,6 +67,27 @@ int main()
   delete[] cnv;
   return err;
 }
+void extend(p_t** pts, size_t s, p_t p) {
+  top::p_t* res = new p_t[s+1];
+  for (size_t i = 0;i < s; i++)
+  {
+    res[i]=(*pts)[i];
+  }
+  res[s] = p;
+  delete [] *pts;
+  *pts = res;
+size_t top::points(const IDraw& d, p_t** pts, size_t s)
+{
+  p_t* res = new p_t[upd_s];
+  extend(pts, s ,p);
+  sixe_t delta = 1;
+  while (d.next(p) != d.begin()) {
+    p = d.next(p);
+    extend(pts, s+delta,p);
+    ++delta;
+  }
+  return delta;
+}
 top::Dot::Dot(int x, int y) :
   IDraw(),
   o{x,y}
